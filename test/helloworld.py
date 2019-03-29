95dd37d8b59123355288b93cfb9161da90e2f7b8
Dan Sun <dsun20@bloomberg.net>
2019-03-29 17:36:12 -0400

    Update helloworld.py

--- test/helloworld.py
+++ test/helloworld.py
@@ -1,7 +1,4 @@
-from bloomberg.blambda.runtime import lambda_logging
-
 def handler(event, context):
-    log = lambda_logging.get_logger("upper")
     log.info(event)
     for item in context.items():
         log.info(item)
